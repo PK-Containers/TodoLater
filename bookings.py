73368aa104130c90d29bb92cbe3ea41fa4473ca1
prabhakm <bhavyakm@gmail.com>
2017-05-05 15:45:22 -0400

    Update bookings.py

--- bookings.py
+++ bookings.py
@@ -37,4 +37,4 @@ def booking_record(username):
     return jsonify(bookings[username])
 
 if __name__ == "__main__":
-    app.run(port=5004, debug=True)
+    app.run(host="0.0.0.0",port=5004, debug=True)
