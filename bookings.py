477fea9b98aea634c33394371a437fc8691810ab
prabhakm <bhavyakm@gmail.com>
2017-05-05 15:49:09 -0400

    Update bookings.py

--- bookings.py
+++ bookings.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 database_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 print database_path
 
-with open("{}/app/database/users.json".format(database_path), "r") as f:
+with open("{}/app/database/bookings.json".format(database_path), "r") as f:
     bookings = json.load(f)
 
 
